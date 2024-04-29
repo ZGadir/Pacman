e262be0442e8babbf38751da4a0144c159f3cf9d
Gadir <zamanbeyovgadir@gmail.com>
2024-04-29 05:17:17 +0000

    succesfully creted a map

--- pacman.py
+++ pacman.py
@@ -1,38 +1,38 @@
 import sys
 import copy
 import pygame
+import math
 
 # Constants
 WIDTH, HEIGHT = 900, 950
-TILE_SIZE = 25
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
 FPS = 60
 color = 'blue'
-BLACK = (0, 0, 0)
-FPS = 60
-TILE_SIZE = 25
 player_x, player_y = 450, 663
 score, lives, powerup = 0, 3, False
 game_over, game_won = False, False
-level = boards
-screen = pygame.display.set_mode([WIDTH, HEIGHT])
-level = copy.deepcopy(boards)
+flicker = False
 PI = math.pi
-
+# R, L, U, D
+turns_allowed = [False, False, False, False]
+direction_command = 0
+player_speed = 2
+eaten_ghost = [False, False, False, False]
+targets = [(player_x, player_y), (player_x, player_y), (player_x, player_y), (player_x, player_y)]
+blinky_dead, inky_dead, clyde_dead, pinky_dead = False, False, False, False
+blinky_box, inky_box, clyde_box, pinky_box = False, False, False, False
+moving = False
+ghost_speeds = [2, 2, 2, 2]
+startup_counter = 0
+tile_height = (HEIGHT - 50) // 32
+tile_width = WIDTH // 30
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 timer = pygame.time.Clock()
 font = pygame.font.Font('freesansbold.ttf', 20)
-
-#ghost_images = {
-#    'blinky': pygame.transform.scale(pygame.image.load('assets/ghost_img/red.png'), (45, 45)),
-#    'pinky': pygame.transform.scale(pygame.image.load('assets/ghost_img/pink.png'), (45, 45)),
-#    'inky': pygame.transform.scale(pygame.image.load('assets/ghost_img/blue.png'), (45, 45)),
-#    'clyde': pygame.transform.scale(pygame.image.load('assets/ghost_img/orange.png'), (45, 45)),
-#    'spooked': pygame.transform.scale(pygame.image.load('assets/ghost_img/spooked.png'), (45, 45)),
-#    'dead': pygame.transform.scale(pygame.image.load('assets/ghost_img/dead.png'), (45, 45))
-#}
-
     
 boards = [
     [6, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5],
@@ -70,19 +70,49 @@ boards = [
     [7, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 8]
 ]
 
-def draw_board():
-    num1 = (HEIGHT - 50) // 32
-    num2 = WIDTH // 30
+level = boards.copy()
 
+def draw_board():
+    num1 = ((HEIGHT - 50) // 32)
+    num2 = (WIDTH // 30)
     for i in range(len(level)):
         for j in range(len(level[i])):
             if level[i][j] == 1:
                 pygame.draw.circle(screen, 'white', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 4)
-            if level[i][j] == 2:
+            if level[i][j] == 2 and not flicker:
                 pygame.draw.circle(screen, 'white', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 10)
             if level[i][j] == 3:
-                pygame.draw.line(screen, color, (j * num2 + (0.5 * num2), i * num1), (j * num2 + (0.5 * num2), i * num1 + num1), 3)
+                pygame.draw.line(screen, color, (j * num2 + (0.5 * num2), i * num1),
+                                 (j * num2 + (0.5 * num2), i * num1 + num1), 3)
+            if level[i][j] == 4:
+                pygame.draw.line(screen, color, (j * num2, i * num1 + (0.5 * num1)),
+                                 (j * num2 + num2, i * num1 + (0.5 * num1)), 3)
+            if level[i][j] == 5:
+                pygame.draw.arc(screen, color, [(j * num2 - (num2 * 0.4)) - 2, (i * num1 + (0.5 * num1)), num2, num1],
+                                0, PI / 2, 3)
+            if level[i][j] == 6:
+                pygame.draw.arc(screen, color,
+                                [(j * num2 + (num2 * 0.5)), (i * num1 + (0.5 * num1)), num2, num1], PI / 2, PI, 3)
+            if level[i][j] == 7:
+                pygame.draw.arc(screen, color, [(j * num2 + (num2 * 0.5)), (i * num1 - (0.4 * num1)), num2, num1], PI,
+                                3 * PI / 2, 3)
+            if level[i][j] == 8:
+                pygame.draw.arc(screen, color,
+                                [(j * num2 - (num2 * 0.4)) - 2, (i * num1 - (0.4 * num1)), num2, num1], 3 * PI / 2,
+                                2 * PI, 3)
+            if level[i][j] == 9:
+                pygame.draw.line(screen, 'white', (j * num2, i * num1 + (0.5 * num1)),
+                                 (j * num2 + num2, i * num1 + (0.5 * num1)), 3)
+    
 
+#ghost_images = {
+#    'blinky': pygame.transform.scale(pygame.image.load('assets/ghost_img/red.png'), (45, 45)),
+#    'pinky': pygame.transform.scale(pygame.image.load('assets/ghost_img/pink.png'), (45, 45)),
+#    'inky': pygame.transform.scale(pygame.image.load('assets/ghost_img/blue.png'), (45, 45)),
+#    'clyde': pygame.transform.scale(pygame.image.load('assets/ghost_img/orange.png'), (45, 45)),
+#    'spooked': pygame.transform.scale(pygame.image.load('assets/ghost_img/spooked.png'), (45, 45)),
+#    'dead': pygame.transform.scale(pygame.image.load('assets/ghost_img/dead.png'), (45, 45))
+#}
             
 
 
@@ -93,11 +123,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game_over = True
-
         screen.fill(BLACK)
         draw_board()
-        # Update game logic and draw other elements here
-
         pygame.display.update()
         timer.tick(FPS)
 
